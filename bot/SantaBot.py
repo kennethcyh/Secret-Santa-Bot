5b8d9808de7c19233b6e89e8fa8d7d17e052b111
kennethcyh <31611324+kennethcyh@users.noreply.github.com>
2018-12-04 17:43:14 +0800

    Adding explicit on clause

--- bot/SantaBot.py
+++ bot/SantaBot.py
@@ -98,7 +98,7 @@ class SantaBot:
                 return
 
             else:
-                currentLinks = self.session.query(Link).join(Link.santa).join(Participant).filter(Link.group_id == chat_id).all()
+                currentLinks = self.session.query(Link).join(Participant.id == Link.santa_id).filter(Link.group_id == chat_id).all()
 
                 print(currentLinks)
 
