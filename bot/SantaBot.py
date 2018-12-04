a8847c1ecc1768976589075fac6516729b94e336
kennethcyh <31611324+kennethcyh@users.noreply.github.com>
2018-12-04 17:27:51 +0800

    Explicit joint

--- bot/SantaBot.py
+++ bot/SantaBot.py
@@ -98,7 +98,7 @@ class SantaBot:
                 return
 
             else:
-                currentLinks = self.session.query(Link).filter(Link.group_id == chat_id).join(Participant).all()
+                currentLinks = self.session.query(Participant).join(Link.santa_id).filter(Link.group_id == chat_id).all()
 
                 print(currentLinks)
 
